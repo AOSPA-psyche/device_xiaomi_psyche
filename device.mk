585fee673aee5ea211b6a6ff286fbc6f4b64daa0
LuK1337 <priv.luk@gmail.com>
2021-05-21 15:32:47 +0200

    psyche: Set default Settings.Global.DEVICE_NAME according to MIUI
    MIUI uses ro.product.vendor.marketname for default device name.
    
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- device.mk
+++ device.mk
@@ -40,6 +40,7 @@ PRODUCT_PACKAGES += NfcTargetOverlay
 PRODUCT_PACKAGES += \
     ApertureTargetOverlay \
     FrameworksTargetOverlay \
+    SettingsProviderTargetOverlay \
     SettingsTargetOverlay \
     SettingsTargetOverlayPsycheCN \
     SettingsTargetOverlayPsycheGL \
