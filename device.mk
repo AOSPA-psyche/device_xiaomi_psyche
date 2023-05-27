385ee619aa8734045cd90d072b039868e72e919a
johnmart19 <johnivan19999@gmail.com>
2023-05-27 04:45:38 +0300

    psyche: Inherit Miui Camera Repository
    - Includes MiSys and cleaned deps
    
    Change-Id: Ibd7d86f5f66d3304e1b743ce333784a53220f8cf

--- device.mk
+++ device.mk
@@ -29,6 +29,8 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+$(call inherit-product-if-exists, device/xiaomi/camera-psyche/miuicamera.mk)
+
 # Dolby Support
 TARGET_USES_MIUI_DOLBY := true
 
