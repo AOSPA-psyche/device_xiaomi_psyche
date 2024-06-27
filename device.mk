4764d6ac5785b9a0dd866aca5c7b4154a9b9c7cb
NotZeetaa <rodrigo2005contente@gmail.com>
2024-06-27 08:40:15 +0000

    psyche: Minimal PixelOS Bringup
    Co-authored-by: Flicker372 <flicker372@outlook.com>

--- device.mk
+++ device.mk
@@ -21,10 +21,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-
 # Camera
 PRODUCT_PACKAGES += \
     libpiex_shim
@@ -34,8 +30,7 @@ PRODUCT_PACKAGES += PsycheNfc
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Sensors
 PRODUCT_COPY_FILES += \
