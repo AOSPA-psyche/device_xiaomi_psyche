512620877456b434b828fb32e0e2081a52ca1032
Flicker372 <flicker372@outlook.com>
2024-09-29 18:53:30 +0800

    psyche: overlay: Import regulatory information and market names
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,8 @@ PRODUCT_PACKAGES += \
     ApertureTargetOverlay \
     FrameworksTargetOverlay \
     SettingsTargetOverlay \
+    SettingsTargetOverlayPsycheCN \
+    SettingsTargetOverlayPsycheGL \
     SystemUITargetOverlay
 
 # Sensors
