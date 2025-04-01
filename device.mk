1e470c4f97195b85c94cbd17a5e96d94a2c7d4ee
Flicker372 <flicker372@outlook.com>
2025-04-01 13:47:06 +0800

    psyche: Commonize DSPVolumeSynchronizer
    * Move DSPVolumeSynchronizer to hardware_xiaomi
    
    Change-Id: I4eb1dd63a8f7486ce4a590f2093cad5b23405a61

--- device.mk
+++ device.mk
@@ -36,10 +36,6 @@ TARGET_USES_MIUI_DOLBY := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
-# DSP Volume Synchronizer
-PRODUCT_PACKAGES += \
-    DSPVolumeSynchronizer
-
 # Overlays
 PRODUCT_PACKAGES += \
     ApertureTargetOverlay \
